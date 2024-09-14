f23554b6e44917738eb469173676f8e478fb35ef
Paulo Llanos <37987149+paulo1403@users.noreply.github.com>
2024-09-14 07:57:25 -0500

    Refactor Location page button text

--- src/pages/Location.tsx
+++ src/pages/Location.tsx
@@ -68,7 +68,7 @@ const Location = () => {
         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-8"
         onClick={() => navigate(-1)}
       >
-        Regresar
+        Aea papi, no me grites
       </button>
     </div>
   );
