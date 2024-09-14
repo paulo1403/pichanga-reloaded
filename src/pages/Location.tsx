3eeb610292ca0d7235b79f90dfeab6022b6c62bf
Paulo Llanos <37987149+paulo1403@users.noreply.github.com>
2024-09-13 21:20:10 -0500

    Refactor Location page to include styled elements and additional information

--- src/pages/Location.tsx
+++ src/pages/Location.tsx
@@ -36,7 +36,9 @@ const Location = () => {
 
   return (
     <div className="flex flex-col justify-center items-center w-full p-4 min-h-screen">
-      <h1 className="text-4xl font-bold text-white mb-8">Ubicación</h1>
+      <h1 className="text-4xl font-bold text-white mb-8 p-2 rounded">
+        Ubicación
+      </h1>
       <iframe
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d747.8376596732202!2d-77.10216924193004!3d-12.071262840523667!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105c977a6e3c26d%3A0xf168c43ae9ecfbfe!2sAcademia%20de%20F%C3%BAtbol%20Dieka%20-%20Sede%20San%20Miguel!5e1!3m2!1sen!2spe!4v1726279711679!5m2!1sen!2spe"
         width="600"
@@ -47,19 +49,21 @@ const Location = () => {
         allowFullScreen
         className="w-full max-w-4xl h-96 border-0"
       ></iframe>
-      <p className="text-white text-lg mt-4">
-        Cada Sábado desde las 11 hasta la 1
-      </p>
-      <p className="text-white text-lg mt-4">
-        Fecha actual: {currentDate.toLocaleString()}
-      </p>
-      <p className="text-white text-lg mt-4">
-        Tiempo hasta el próximo sábado a las 11 AM: {timeUntilNextSaturday}
-      </p>
-      <p className="text-white text-lg mt-4">
-        ¡No faltes! Sé puntual para que no te pierdas ni un minuto de la
-        pichanga. CTM!
-      </p>
+      <div className="bg-black bg-opacity-50 mt-2 p-4 rounded">
+        <p className="text-white text-lg mt-4 p-2 rounded">
+          Cada Sábado desde las 11 hasta la 1
+        </p>
+        <p className="text-white text-2xl mt-4 p-2 rounded rainbow-text">
+          Fecha actual: {currentDate.toLocaleString()}
+        </p>
+        <p className="text-white text-2xl mt-4 p-2 rounded rainbow-text">
+          Tiempo hasta el próximo sábado a las 11 AM: {timeUntilNextSaturday}
+        </p>
+        <p className="text-white text-lg mt-4 p-2 rounded">
+          ¡No faltes! Sé puntual para que no te pierdas ni un minuto de la
+          pichanga. CTM!
+        </p>
+      </div>
       <button
         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-8"
         onClick={() => navigate(-1)}
